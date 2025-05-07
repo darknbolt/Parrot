7e1f8e1690c28022a8842fb2239a1b2478fbe05a
DarkNBolt <mgrombiz@gmail.com>
2025-05-07 02:10:43 +0200

    Client Fields

--- src/Components/Client.java
+++ src/Components/Client.java
@@ -1,11 +1,24 @@
 package Components;
 
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Client {
     private String nickname;
+    private Socket socket;
+    private DatagramSocket datagramSocket;
+    private DatagramPacket datagramPacket;
+    private InetAddress ip;
 
     //TODO: More Data Load
 
     public Client(String nickname){
         this.nickname = nickname;
+        this.socket = null;
+        this.datagramSocket = null;
+        this.datagramPacket = null;
+        this.ip = InetAddress.getLoopbackAddress();
     }
 }
